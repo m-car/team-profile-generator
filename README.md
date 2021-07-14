3f24f70f43d3fa38c9ebb6415fe6cc24446bb09f
Marko Caric <pbmc117@gmail.com>
2021-07-13 22:17:57 -0700

    Intern and Intern.test are built, all tests passing. Added to README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # team-profile-generator
+
+
+## Description
+This is a Team Profile generator that uses NPM, inquirer.js, jest and javascript to prompt the user questions about Employee information for their company. Then a webpage will be created, displaying the different employee's information. This was built with Test Driven Development methodologies.
+
+## Installation
+- Run NPM install for inquirer and jest via your console as the node_modules is not included/tracked. Type "npm i inquirer" and "npm i jest"
+
+## Testing
+- To run the tests, enter "npm run test" into the console.
+
+## Usage
+1. Type "node index.js" into your console.
+
+    <!-- <img src="images/Capture1.PNG"> -->
+
+2. Follow the prompts on screen.
+
+    <!-- <img src="images/Capture2.PNG"> -->
+
+3. A website is generated in the Output folder. 
+
+    <!-- <img src="images/Capture3.PNG"> -->
+
+
+
+
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,32 @@
-describe("Intern", ()=>{
-    it.todo("nothing here yet")
+const Intern = require("../lib/Intern.js")
+describe("Intern class", ()=>{
+    describe("constructor", () => {
+        //arrange
+        it("Sets name, id , email, school", ()=>{
+            const name = "marko"
+            const id = 5;
+            const email = "marko@gmail.com";
+            const school = "UCSD"
+
+            //act 
+            const intern = new Intern(name, id, email , school)
+            
+            //assert
+            expect(intern).toEqual({
+                name:"marko", 
+                id: 5, 
+                email: "marko@gmail.com",
+                role: "Intern",
+                school: "UCSD"
+            })
+
+
+        })
+
+    })
+    describe("getRole", () =>{
+        const intern = new Intern("marko", 5, "marko@gmail.com");
+        const result = intern.getRole();
+        expect(result).toEqual("Intern");
+    })
 })
